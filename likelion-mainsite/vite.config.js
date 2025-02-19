cad54a1c72885811a94f9ae79c9739342036f169
hello-yujin <yuuuujiiiin@naver.com>
2025-02-19 18:46:01 +0900

    Feat: 파일 기본 설정 변경

--- likelion-mainsite/vite.config.js
+++ likelion-mainsite/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import path from 'node:path'; // ✅ ESM 방식으로 변경
+import { fileURLToPath } from 'url';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+});
