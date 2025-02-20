405021a7f61274e56d7121d6acb07da069d103ad
yeilkk <yeil99@naver.com>
2025-02-21 01:44:00 +0900

    feat: 파트별 소개 반응형 구현

--- likelion-mainsite/src/components/Part.jsx
+++ likelion-mainsite/src/components/Part.jsx
@@ -1,36 +1,25 @@
 import React from 'react';
 import './Part.css';
 import PartTable from './PartTable';
+import { useState } from 'react';
 
 const Part = () => {
+    const [part, setPart] = useState('web');
 
-    const weeks = [
-        { week: 1, session: "JAVA 초기설정 및 기초 개념" },
-        { week: 2, session: "JAVA 개념 마무리" },
-        { week: 3, session: "JAVA 심화" },
-        { week: 4, session: "Spring(1) 초기설정" },
-        { week: 5, session: "Spring (2) 스프링 기초 및 MVC 패턴" },
-        { week: 6, session: "Spring (3) 스프링 심화" },
-        { week: 7, session: "Spring (4) DB 설정 및 JPA" },
-        { week: 8, session: "Spring (5) 게시판 CRUD " },
-        { week: 9, session: "Spring (6) 게시판 CRUD 마무리" },
-        { week: 10, session: "WEB-SERVER 크로스오버 1" },
-        { week: 11, session: "WEB-SERVER 크로스오버 2" },
-        { week: 12, session: "로드밸런싱과 스케일링" },
-        { week: 13, session: "Redis와 캐시 최적화" },
-        { week: 14, session: "Kafka 및 비동기 메시징" },
-        { week: 15, session: "서버 성능 모니터링 및 최적화" },
-        { week: 16, session: "최종 프로젝트 발표" },
-    ]
+    const handlePartChange = (part) => {
+        setPart(part);
+    }
 
     return (
         <div className="part-container">
             <div className='part-title'>파트별 소개</div>
-            <div className='part-box'>
-                <div className='part-subtitle'>웹</div>
-                <div className='part-subtitle'>서버</div>
+            <div className='part-box-container'>
+                <div className='part-box'>
+                    <div className={`part-subtitle ${part === 'web' ? 'active-part' : ''}`} onClick={() => handlePartChange("web")}>웹</div>
+                    <div className={`part-subtitle ${part === 'server' ? 'active-part' : ''}`} onClick={() => handlePartChange("server")}>서버</div>
+                </div>
+                <PartTable part={part} setPart={setPart}/>
             </div>
-            <PartTable weeks={weeks}/>
         </div>
     );
 };
--- likelion-mainsite/src/components/PartTable.jsx
+++ likelion-mainsite/src/components/PartTable.jsx
@@ -1,17 +1,55 @@
-import React from "react";
+import React, { use, useEffect, useState } from "react";
 import "./PartTable.css";
 
-const PartTable = ({ weeks }) => {
+const PartTable = ({ part, setPart }) => {
+  const serverWeeks = [
+    { week: 1, session: "공통 세션" },
+    { week: 2, session: "JAVA <br/> 초기설정 및 기초 개념" },
+    { week: 3, session: "JAVA 개념 마무리" },
+    { week: 4, session: "JAVA 심화" },
+    { week: 5, session: "Spring(1) 초기설정" },
+    { week: 6, session: "Spring (2) <br/> 스프링 기초 및 MVC 패턴" },
+    { week: 7, session: "Spring (3) <br/> 스프링 심화" },
+    { week: 8, session: "Spring (4)<br/> DB 설정 및 JPA" },
+    { week: 9, session: "Spring (5) <br/> 게시판 CRUD " },
+    { week: 10, session: "Spring (6) <br/> 게시판 CRUD 마무리" },
+    { week: 11, session: "WEB-SERVER <br/> 크로스오버 1" },
+    { week: 12, session: "WEB-SERVER <br/> 크로스오버 2" }
+  ]
+
+  const webWeeks = [
+    { week: 1, session: "공통 세션" },
+    { week: 2, session: "HTML/CSS" },
+    { week: 3, session: "HTML/CSS" },
+    { week: 4, session: "JavaScript" },
+    { week: 5, session: "JavaScript" },
+    { week: 6, session: "React(1)" },
+    { week: 7, session: "React(2)" },
+    { week: 8, session: "React(3)" },
+    { week: 9, session: "React(4)" },
+    { week: 10, session: "React(5)" },
+    { week: 11, session: "WEB-SERVER <br/> 크로스오버 1" },
+    { week: 12, session: "WEB-SERVER <br/> 크로스오버 2" }
+  ]
+
+  const [partWeeks, setPartWeeks] = useState(part === "web" ? webWeeks : serverWeeks);
+
+  useEffect(() => {
+    setPartWeeks(part === "web" ? webWeeks : serverWeeks);
+  }
+  , [part]);
+
   return (
     <div className="part-table-container">
       {/* 왼쪽 (1~8주차) */}
-      <div className="part-table part-table-left">
+      <div className="part-table">
         <table>
           <tbody>
-            {weeks.slice(0, 8).map(({ week, session }) => (
+            {partWeeks.slice(0, 6).map(({ week, session }) => (
               <tr key={week}>
                 <td className="part-week">week {week}</td>
-                <td className="part-session">{session}</td>
+                <td className="part-session">{session.replace(/<br\/>/g, " ")}</td>
+                <td className="part-session2" dangerouslySetInnerHTML={{ __html: session }}></td>
               </tr>
             ))}
           </tbody>
@@ -19,13 +57,14 @@ const PartTable = ({ weeks }) => {
       </div>
 
       {/* 오른쪽 (9~16주차) */}
-      <div className="part-table part-table-right">
+      <div className="part-table">
         <table>
           <tbody>
-            {weeks.slice(8, 16).map(({ week, session }) => (
+            {partWeeks.slice(6, 13).map(({ week, session }) => (
               <tr key={week}>
                 <td className="part-week">week {week}</td>
-                <td className="part-session">{session}</td>
+                <td className="part-session">{session.replace(/<br\/>/g, " ")}</td>
+                <td className="part-session2" dangerouslySetInnerHTML={{ __html: session }}></td>
               </tr>
             ))}
           </tbody>
