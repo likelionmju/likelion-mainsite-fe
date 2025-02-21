bdbb5443654e3dd9346e6b05cb58b79825af0a92
yeilkk <yeil99@naver.com>
2025-02-22 01:01:02 +0900

    feat: 멋쟁이사자처럼  소개

--- likelion-mainsite/src/pages/Home.jsx
+++ likelion-mainsite/src/pages/Home.jsx
@@ -3,11 +3,13 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import './Home.css';
 import Part from '@/components/Part';
+import Introduction from '@/components/Introduction';
 
 const Home = () => {
 	return (
 		<div className="Home">
 			<Header />
+			<Introduction />
 			<Part/>
 			<Footer />
 		</div>
