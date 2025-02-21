f2068e7db3816f2a55b629b33db9ebaefd8ac0c2
hello-yujin <yuuuujiiiin@naver.com>
2025-02-21 17:09:40 +0900

    Feat: Admin 페이지 접속을 위한Router 재설정

--- likelion-mainsite/src/routes/Router.jsx
+++ likelion-mainsite/src/routes/Router.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '@/pages/Home';
 import Login from '@/pages/Login';
 import RecruitPage from '@/pages/Recruit';
+import AdminLogin from '@/pages/AdminLogin';
+import AdminPage from '@/pages/Admin';
+import AdminRoute from './AdminRouter';
+import CheckResult from '@/pages/CheckResult';
 // import DefaultLayout from '@/components/layout';
 
 const Router = () => {
@@ -13,6 +17,13 @@ const Router = () => {
         <Route path="/" element={<Home />} />
         <Route path="/recruit" element={<RecruitPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/checkresult" element={<CheckResult />} />
+        <Route path="/adminlogin" element={<AdminLogin />} />
+        
+        {/* 관리자 전용 페이지 - AdminRoute로 보호 */}
+        <Route element={<AdminRoute />}>
+          <Route path="/admin" element={<AdminPage />} />
+        </Route>
         {/* </Route> */}
       </Routes>
     </BrowserRouter>
