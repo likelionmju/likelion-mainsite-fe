3e5ab95173afda89ef2afd1ccd0fac7f2f572c70
hello-yujin <yuuuujiiiin@naver.com>
2025-02-23 16:49:24 +0900

    Feat: Home 페이지 퍼블리싱

--- likelion-mainsite/src/pages/Home.jsx
+++ likelion-mainsite/src/pages/Home.jsx
@@ -3,10 +3,11 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import './Home.css';
 import YearPlan from '@/components/YearPlan';
-import character from '../assets/character.svg'
+// import character from '../assets/character.svg'
+import character from '../assets/character4.svg'
 import Apply from '@/components/Apply';
 import Card from '@/components/Card';
-import fire_icon from '../assets/fire_icon.svg';
+import fire_icon from '@/assets/fire_icon.svg';
 import chat_icon from '../assets/chat_icon.svg';
 import crown_icon from '../assets/crown_icon.svg';
 import Part from '@/components/Part';
@@ -17,21 +18,26 @@ import monitor_icon from "../assets/monitor_icon.svg";
 import heart_icon from "../assets/heart_icon.svg";
 import recruit_date_line from "../assets/recruit_date_line.svg";
 import RecruitDate from '@/components/RecruitDate';
+import Banner from '@/components/Banner.jsx';
+import Introduction from '@/components/Introduction';
 
 const Home = () => {
 	return (
 		<div className="Home">
-			<Header />
+			{/* <Header /> */}
 			{/* 배너 */}
-			<div className="banner">
+			<Banner character={character} />
+			{/* <Introduction/> */}
+			{/* <Introduction/> */}
+			{/* <div className="banner">
 				<div className="banner_title_container">
 					<img className="character" src={character} />
 					<p className="banner_title">LIKELION MJU</p>
-				</div>
-				{/* <img className="character" src={character} />
-				<p className="banner_title">LIKELION MJU</p> */}
-				<Apply />
-			</div>
+				</div> */}
+				{/* {/* <img className="character" src={character} /> */}
+				{/* <p className="banner_title">LIKELION MJU</p> */}
+				{/* <Apply /> */}
+			{/* </div> */}
 
 			{/* 멋사 소개 */}
 			{/* <div className="why">
@@ -141,7 +147,7 @@ const Home = () => {
 				</div>
 			</div> */}
 
-			<Footer />
+			{/* <Footer /> */}
 		</div>
 	);
 };
