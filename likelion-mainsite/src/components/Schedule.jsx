736b9f96bdd30c81893a99a01c58dc2730dc9ab5
yeilkk <yeil99@naver.com>
2025-02-25 18:52:53 +0900

    feat: 배너, 연간일정, 모집일정 반응형 수정

--- likelion-mainsite/src/components/Schedule.jsx
+++ likelion-mainsite/src/components/Schedule.jsx
@@ -4,30 +4,34 @@ import React from 'react';
 const Button = () => {
 	return (
         <div className="recruit_date">
-        <p className="recruit_date_title">모집 일정</p>
+        <div className="recruit_date_title">모집 일정</div>
         <div className="recruit_date_div">
-                <div>
+                <div className='recruit_date_container'>
                     <p className="recruit_date_content_bold">02/26 - 03/09</p>
                     <div className="recruit_date_box">
-                        <span className="recruit_date_content_medium">서류 전형</span>
+                        서류전형
+                        {/* <span className="recruit_date_content_medium">서류 전형</span> */}
                     </div>
                 </div>
-                <div>
+                <div className='recruit_date_container'>
                     <p className="recruit_date_content_bold">03/10</p>
                     <div className="recruit_date_box">
-                        <span className="recruit_date_content_medium">서류 합격 발표</span>
+                        서류 합격 발표
+                        {/* <span className="recruit_date_content_medium">서류 합격 발표</span> */}
                     </div>
                 </div>
-                <div>
+                <div className='recruit_date_container'>
                     <p className="recruit_date_content_bold">03/12 - 03/14</p>
                     <div className="recruit_date_box">
-                        <span className="recruit_date_content_medium">면접 전형</span>
+                        면접 전형
+                        {/* <span className="recruit_date_content_medium">면접 전형</span> */}
                     </div>
                 </div>
-                <div>
+                <div className='recruit_date_container'>
                     <p className="recruit_date_content_bold">03/15</p>
                     <div className="recruit_date_box">
-                        <span className="recruit_date_content_medium">최종 합격 발표</span>
+                        최종 합격 발표
+                        {/* <span className="recruit_date_content_medium">최종 합격 발표</span> */}
                     </div>
                 </div>
         </div>
